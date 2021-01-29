3c7d9e46688ae13c2ff1905e6d88c2bc09e04e9c
jnutterdev <hello@jnutterdev.com>
2021-01-29 11:26:24 -0500

    Simple app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+// imports useState for React hooks, ONLY works inside of a function component
+
+// useState hooks MUST run in the same order, and can NOT be nested or used conditionally
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+ const [ state, setState ] = useState({ count: 4, theme: 'blue'});
+ const count = state.count;
+ const theme = state.theme;
+
+  function decrementCount() {
+    
+      setState(prevState => {
+        return { ...prevState, count: prevState.count - 1 };
+    })
+    
+}
+
+function incrementCount() {
+  setState(prevState => {
+    return { ...prevState, count: prevState.count + 1 };
+})
+}
+
+   return (
+    <>
+      <button onClick={decrementCount}> - </button>
+        <span>{count} {theme}</span>
+      <button onClick={incrementCount}> + </button><br />
+    
+    </>
   );
 }
 
